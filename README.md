a4ca139ebafe95f0a52a5c708b528264017283cf
arthur12320 <arthurraragao@hotmail.com>
2024-10-25 00:17:44 -0300

    ft: added shadcdn

--- README.md
+++ README.md
@@ -2,7 +2,8 @@ This is a a next15 project of a games collection.
 
 TODO:
 
-- [ ] setup tailwindCSS
+- [x] setup tailwindCSS
+- [ ] setup chadcdn
 - [x] setup DB [drizzle-orm + vercel postgres]
 - [ ] setup image upload [vercel blob]
 - [ ] setup auth
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { getUsers } from "@/lib/queries";
 
 export default async function Home() {
   const users = await getUsers();
   return (
     <div>
-      <h1>React test</h1>
+      <h1 className="text-4xl font-bold">React test</h1>
+      <Button>Click me</Button>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.username}</li>
