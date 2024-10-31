8483d69a48c407dfe679b65ce551b388dc89de33
arthur12320 <arthurraragao@hotmail.com>
2024-10-31 00:13:41 -0300

    ft: updated next-auth to 5.0

---
+++ auth.ts
@@ -0,0 +1,10 @@
+import db from "@/db"
+import { env } from "@/env/server"
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [Google({ clientId: env.GOOGLE_CLIENT_ID, clientSecret: env.GOOGLE_CLIENT_SECRET }) as any],
+    adapter: DrizzleAdapter(db),
+})
