aefb217405efcf756660ecdfab7f62fd6143b579
Arthur R Aragão <ara@cesar.org.br>
2025-02-17 14:24:04 -0300

    ft: simplified collections listing

--- src/app/mycollections/page.tsx
+++ src/app/mycollections/page.tsx
@@ -4,10 +4,10 @@ import { SelectCollectionWithUser } from "@/db/schema/collections";
 import { getCollections } from "@/lib/queries";
 
 export default async function TestPage() {
-  const collections = (await getCollections()) as SelectCollectionWithUser[];
-  return (
-    <Card className="mx-auto max-w-2xl mt-10">
-      <CollectionListing collections={collections} />
-    </Card>
-  );
+    const collections = (await getCollections()) as SelectCollectionWithUser[];
+    return (
+        <Card className="mx-auto max-w-2xl mt-10">
+            <CollectionListing collections={collections} />
+        </Card>
+    );
 }
